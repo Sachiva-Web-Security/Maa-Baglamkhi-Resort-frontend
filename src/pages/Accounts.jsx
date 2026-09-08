d00070cd78b3bff069b4d46c8d023c4dc9974da2
Zack-knight <2021eo260hi@mitsgwl.ac.in>
2026-09-08 14:53:13 +0530

    Fix import statement to include React in Accounts.jsx

--- src/pages/Accounts.jsx
+++ src/pages/Accounts.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import {
   FaArrowLeft,
